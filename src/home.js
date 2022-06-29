70e825ef2006a196fbe7a85ffec5b4e7908ebd21
Will Downey <willd20@vt.edu>
2022-06-29 19:15:10 -0400

    Review section

--- src/home.js
+++ src/home.js
@@ -2,6 +2,24 @@ let content = document.getElementById('content');
 
 //loads home page
 function loadHome() {
+    let heading = document.createElement('div');
+    heading.classList.add('heading');
+
+    let review = document.createElement('div');
+    review.classList.add('review');
+    review.classList.add('content-section');
+    let reviewText = document.createElement('p');
+    reviewText.innerHTML = '"The best restaurant in the world!"';
+    review.appendChild(reviewText);
+    content.appendChild(review);
+
+    let hours = document.createElement('div');
+    hours.classList.add('hours');
+    hours.classList.add('content-section');
+
+    let location = document.createElement('div');
+    location.classList.add('location');
+    location.classList.add('content-section');
 }
 
 export {loadHome};
